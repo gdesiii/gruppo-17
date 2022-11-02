e43c438578d96fd67f15828f213a92e262376c72
arian <arian@LAPTOP-9CJ5093N.homenet.telecomitalia.it>
2022-11-02 11:48:16 +0100

    aggiunta funzione per istogrammi

---
+++ confronto_PDF.m
@@ -0,0 +1,18 @@
+function  stima_PDF(variabile_1, variabile_2,n_bins)
+
+bin_edges = linspace(min([variabile_1 ; variabile_2 ]),max([variabile_1 ; variabile_2]), n_bins+1);
+
+bin_centers = zeros(n_bins,1);
+    
+for idx = 1:n_bins
+    bin_centers(idx) = (bin_edges(idx+1)+bin_edges(idx))/2;
+end
+
+figure
+histogram(variabile_1 , bin_centers)
+hold on
+histogram(variabile_2 , bin_centers)
+hold off
+
+end
+
--- find_ecg_peaks.m
+++ find_ecg_peaks.m
@@ -1,11 +1,11 @@
 function [peaks, xpeaks] = find_ecg_peaks(segnale, freq, m)
 
-thr = find_thr(segnale, m);
 
 if m ~= 0
-[peaks, xpeaks] = findpeaks(segnale, freq, 'MinPeakHeight',  thr);
+    thr = find_thr(segnale, m);
+    [peaks, xpeaks] = findpeaks(segnale, freq, 'MinPeakHeight',  thr);
 else
-[peaks, xpeaks] = findpeaks(segnale);
+    [peaks, xpeaks] = findpeaks(segnale);
 end
 end
 
